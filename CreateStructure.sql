8c09089c97d358ad87c643d1a9f461cd35495b1f
anshexa <lilimahiya@gmail.com>
2023-07-23 13:48:50 +0300

    Создание таблиц

---
+++ CreateStructure.sql
@@ -0,0 +1,35 @@
+use purchasesdb;
+
+create table dbo.SKU
+(
+	ID int identity
+	,Code as concat('s', convert(varchar, ID)) persisted unique
+	,Name varchar(150) not null
+
+	,constraint PK_SKU primary key (ID)
+);
+
+create table dbo.Family
+(
+	ID int identity
+	,SurName varchar(255) not null
+	,BudgetValue decimal(18, 2) not null
+
+	,constraint PK_Family primary key (ID)
+);
+
+create table dbo.Basket
+(
+	ID int identity
+	,ID_SKU int not null
+	,ID_Family int not null
+	,Quantity int not null
+	,Value decimal(18, 2) not null
+	,PurchaseDate date not null default(convert (date, sysdatetime()))
+	,DiscountValue decimal(18, 2)
+
+	,constraint FK_Basket_SKU foreign key (ID_SKU) references dbo.SKU (ID)
+	,constraint FK_Basket_Family foreign key (ID_Family) references dbo.Family (ID)
+	,constraint CHK_Basket_Quantity check(Quantity > 0)
+	,constraint CHK_Basket_Value check(Value > 0)
+);
